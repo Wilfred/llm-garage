1107877a3e034d8c5fd8381f6d9df327cd599533
Wilfred Hughes <me@wilfred.me.uk>
2026-09-06 00:38:45 +0100

    Persist sessions and events in SQLite

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,7 +2,10 @@ import "reflect-metadata";
 import path from "node:path";
 import { DataSource } from "typeorm";
 import { RepoEntity } from "../entities/repo";
+import { RunEventEntity } from "../entities/run-event";
+import { SessionEntity } from "../entities/session";
 import { Setting } from "../entities/setting";
+import { TurnEntity } from "../entities/turn";
 
 export const databaseFilename = "app.db";
 
@@ -10,7 +13,7 @@ export function createAppDataSource(dataDir: string): DataSource {
   return new DataSource({
     type: "better-sqlite3",
     database: path.join(dataDir, databaseFilename),
-    entities: [Setting, RepoEntity],
+    entities: [Setting, RepoEntity, SessionEntity, TurnEntity, RunEventEntity],
     synchronize: true,
     enableWAL: true,
     timeout: 5_000,
